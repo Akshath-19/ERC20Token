f275481447c32e250eed7f755c88adff65931cc7
Akshath-19 <akshath19a@gmail.com>
2025-08-20 09:10:53 +0530

    Save work before installing forge-std

--- src/MyToken.sol
+++ src/MyToken.sol
@@ -5,7 +5,7 @@ import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 
 contract MyToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
+    constructor(uint256 initialSupply) ERC20("RCB", "MTK") {
         _mint(msg.sender, initialSupply * 10 ** decimals());
     }
 }
--- test/test/MyToken.t.sol
+++ test/test/MyToken.t.sol
@@ -1,27 +1,63 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+    // SPDX-License-Identifier: MIT
+    pragma solidity ^0.8.20;
 
+// If using Foundry, ensure forge-std is installed via: forge install foundry-rs/forge-std
 import "forge-std/Test.sol";
-import {MyToken} from "../../src/MyToken.sol"; 
-contract MyTokenTest is Test {
-    MyToken token;
+import {MyToken} from "src/MyToken.sol";
 
-    function setUp() public {
-        token = new MyToken(1000); // 1000 tokens initial supply
-    }
 
-    function testNameAndSymbol() public view {
-        assertEq(token.name(), "MyToken");
-        assertEq(token.symbol(), "MTK");
-    }
+    // ...existing code...
 
-    function testInitialSupply() public view {
-        assertEq(token.totalSupply(), 1000 * 10 ** token.decimals());
-    }
+    contract MyTokenTest is Test {
+        MyToken token;
+        address owner = address(this);
+        address spender = address(0xBEEF);
+        address receiver = address(0xCAFE);
 
-    function testTransfer() public {
-        address receiver = address(0xBEEF);
-        token.transfer(receiver, 100 * 10 ** token.decimals());
-        assertEq(token.balanceOf(receiver), 100 * 10 ** token.decimals());
+        function setUp() public {
+            token = new MyToken(1000); // initial supply = 1000 MTK
+        }
+
+        function testNameAndSymbol() public view {
+            assertEq(token.name(), "RCB");
+            assertEq(token.symbol(), "MTK");
+        }
+
+        function testInitialSupply() public view {
+            assertEq(token.totalSupply(), 1000 * 10 ** token.decimals());
+        }
+
+        function testTransfer() public {
+            token.transfer(spender, 100 * 10 ** token.decimals());
+            assertEq(token.balanceOf(spender), 100 * 10 ** token.decimals());
+        }
+
+        // ----------- Day 3 New Tests -----------
+
+        function testApproveAndAllowance() public {
+            token.approve(spender, 200 * 10 ** token.decimals());
+            assertEq(token.allowance(owner, spender), 200 * 10 ** token.decimals());
+        }
+
+        function testTransferFromWithAllowance() public {
+            token.approve(spender, 100 * 10 ** token.decimals());
+            vm.prank(spender);
+            token.transferFrom(owner, receiver, 100 * 10 ** token.decimals());
+            assertEq(token.balanceOf(receiver), 100 * 10 ** token.decimals());
+        }
+
+        function test_Revert_TransferWithoutEnoughAllowance() public {
+            vm.prank(spender);
+            vm.expectRevert(); // expect revert since no allowance
+            token.transferFrom(owner, receiver, 50 * 10 ** token.decimals());
+        }
+
+        function test_Revert_TransferWithoutEnoughBalance() public {
+            vm.startPrank(spender);
+            token.approve(spender, 5000 * 10 ** token.decimals()); // give allowance first
+            vm.expectRevert(); // expect revert due to insufficient balance
+            token.transferFrom(owner, receiver, 5000 * 10 ** token.decimals());
+            vm.stopPrank();
+        }
+
     }
-}
